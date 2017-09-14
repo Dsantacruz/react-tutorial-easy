19e16f33cb0ac1af42d9aff56400d3dc6f02613c
Diego Santa Cruz <diegomiguel114@gmail.com>
2017-09-14 00:23:51 -0500

    add second component for test

---
+++ todoApp/primeraappreact/src/Segundo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Segundo extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h4>Saludo desde un segundo componente</h4>
+            </div>
+        )
+    }
+}
+
+export default Segundo;
